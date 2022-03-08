e1f687ede9465a4597aabf802cb7e682fe284c13
Mario Perez-Jordan <mpjmario@gmail.com>
2022-03-08 10:10:09 -0500

    Initial commit MongoDB project

---
+++ src/test/java/com/techprimers/mongodb/springbootmongodbexample/SpringBootMongodbExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techprimers.mongodb.springbootmongodbexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMongodbExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
